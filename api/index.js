6afb961b94c56fbcf3b6496fed3875efa5953a2e
SihabMolla11 <sihabuddinmolla11@gmail.com>
2024-09-25 00:37:28 +0600

    update

--- api/index.js
+++ api/index.js
@@ -4,11 +4,19 @@ import express from "express";
 import { connectWithDatabase } from "./db.js";
 import api_route from "./router.js";
 
+const corsConfig = {
+  origin: ".",
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+};
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 const db_url = process.env.DB_URL;
+
+app.options("", cors(corsConfig));
 
 app.use(
   cors({
@@ -25,7 +33,7 @@ app.get("/", (req, res) => {
 
 app.use("/api", api_route);
 
-app.listen(() => {
+app.listen(port, () => {
   connectWithDatabase(db_url);
   console.log("server is running on", port);
 });
